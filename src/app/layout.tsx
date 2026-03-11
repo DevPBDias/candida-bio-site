201f782612a194e79911147f98fef1e68e89f639
DevPBDias <devpbdias@gmail.com>
2026-03-11 00:24:44 -0300

    feat: Implement initial Next.js root layout with metadata and Vercel analytics/speed insights integration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { inter, newYork } from "./fonts";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,10 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} ${newYork.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${newYork.variable} antialiased`}>
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
